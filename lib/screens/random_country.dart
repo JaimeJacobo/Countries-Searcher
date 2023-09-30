362284e58ae96f612a862310679684efeb1542ab
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-09-30 10:42:41 +0900

    Created API call for getting all countries

--- lib/screens/random_country.dart
+++ lib/screens/random_country.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class RandomCountry extends StatefulWidget {
   const RandomCountry({super.key});
@@ -8,6 +11,37 @@ class RandomCountry extends StatefulWidget {
 }
 
 class _RandomCountryState extends State<RandomCountry> {
+  List<dynamic> countries = [];
+
+  Future<void> fetchCountries() async {
+    print('fetch being done');
+    const url = 'https://restcountries.com/v3.1/all';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        List<dynamic> responseData = jsonDecode(response.body);
+        setState(() {
+          countries = responseData;
+        });
+        print('API call successful: $countries');
+      } else {
+        // API call failed
+        print('Failed to load data: ${response.statusCode}');
+      }
+    } catch (error) {
+      // Error during API call
+      print('Error during API call: $error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCountries();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
