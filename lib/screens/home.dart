07d01a44510a708d5c68f8e1e93412ea375f09c8
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-09-30 10:17:32 +0900

    Created navbar

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'This is the HOME Page',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
