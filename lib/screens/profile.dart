66daef3ebce62c18b9c38021ea76f3cf13221f53
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-10-02 07:16:00 +0900

    Connecting Profile page to firebase

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Firebase
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -14,9 +17,29 @@ class _ProfileState extends State<Profile> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
+          children: <Widget>[
+            const Text(
               'This is the PROFILE Page',
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  NavigatorState navigatorState = Navigator.of(context);
+                  try {
+                    await FirebaseAuth.instance.signOut();
+                    print(FirebaseAuth.instance.currentUser);
+                    navigatorState.pushNamed('/login');
+                  } catch (err) {
+                    print('Error in log out');
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.blueGrey), // Set the background color here
+                ),
+                child: const Text('Log Out'),
+              ),
             ),
           ],
         ),
